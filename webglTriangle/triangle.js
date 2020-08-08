c75ef82bf99a995b2c7559f4610a7228e90a4cd1
memon07 <memonshoyeb26@gmail.com>
2020-08-08 20:18:41 +0530

    changes on traingle

--- webglTriangle/triangle.js
+++ webglTriangle/triangle.js
@@ -4,34 +4,57 @@ var gl;
 
 function start(){
   var canvas = document.getElementById("renderCanvas");
-  g1 = canvas.getContext("webgl2");
+  gl = canvas.getContext("webgl2");
   var triangleVertices = [
     1.0,-1.0,0.0,
     0.0,1.0,0.0,
     -1.0,-1.0,0.0
   ];
-  var traingleVBuffer = g1.createBuffer();
-  g1.bindBuffer(g1.ARRAY_BUFFER,traingleVBuffer);
-  g1.bufferData(g1.ARRAY_BUFFER, new Float32Array(triangleVertices),g1.STATIC_DRAW);
+  var traingleVBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER,traingleVBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices),gl.STATIC_DRAW);
   
   var triangleColors = [
     1.0,0.0,0.0,1.0,
     0.0,1.0,0.0,1.0,
     0.0,0.0,1.0,1.0,
   ];
-  var traingleCBuffer = g1.createBuffer();
-  g1.bindBuffer(g1.ARRAY_BUFFER,traingleCBuffer);
-  g1.bufferData(g1.ARRAY_BUFFER, new Float32Array(triangleColors),g1.STATIC_DRAW);
+  var traingleCBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER,traingleCBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleColors),gl.STATIC_DRAW);
   
-  getAndCompileVertexShader("vertexShader");
+  var vertexShader = getAndCompileShader("vertexShader");
+  var fragmentShader= getAndCompileShader("fragmentShader");
+  var shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert('Error in program');
+  }
+
+  gl.useProgram(shaderProgram);
 }
 
-function getAndCompileVertexShader(id){
+function getAndCompileShader(id){
   var shader;
   var shaderElement = document.getElementById(id);
-  var shaderText = shaderElement.text;
-  shader = g1.createShader(g1.VERTEX_SHADER);
-  g1.shaderSource(shader,shaderText);
+  var shaderText = shaderElement.text.trim();
+  console.log(':::', shaderText);
+  if(id === 'vertexShader')
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  else if(id === 'fragmentShader')
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(shader,shaderText);
+  gl.compileShader(shader);
+
+  if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.log(gl.getShaderInfoLog(shader))
+    alert(gl.getShaderInfoLog(shader));
+    return null;
+  }
   
   return shader;
 }
